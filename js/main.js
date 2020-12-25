60089f32e609df88abce9895bd7b83c1a896aaec
Marcos Ribeiro <slproger@gmail.com>
2020-12-25 00:27:54 -0300

    usando variaveis e console.log

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-alert("Meu primeiro JS")
+idade = 48
+idade2 = 10
+nome = "Marcos Ribeiro"
+//alert("Meu primeiro JS")
+console.log(idade + idade2);
+console.log(nome + " tem "+idade+" anos de idade");
+console.log(idade2*3);
