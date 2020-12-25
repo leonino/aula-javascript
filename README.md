56c519c51d93f0337436417385bd28b28e79b204
Marcos Ribeiro <slproger@gmail.com>
2020-12-25 01:00:01 -0300

    trabalhando com arrays e objetos

--- README.md
+++ README.md
@@ -5,12 +5,18 @@ instrodução ao Javascipt
 
 as variaveis em js não precisa definir o tipo para atribuir diretamente o valor a variável..
 
-nome = "Marcos"
-idade = 48
+var nome = "Marcos";
+var idade = 48;
 
 o JS armazenda os dados nas variaveis de forma dinamica.
 
 ## usando o console do navedador para DEBUGAR o seu cógido
 
 use o comando sonsole.log(<variavel> or <expressão>) para exibir o valor no console do navegador
+
+console.log(nome+" tem "+idade+" anos de idade");
+
+o comando a cima irá produzir a seguinte saída no console!
+
+>marcos tem 48 anos de idade
 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-idade = 48
-idade2 = 10
-nome = "Marcos Ribeiro"
+/*
+var frutas = []
+var fruta1 = {"nome":"Maçã","Cor": "vermelha"}
+var fruta2 = {"nome":"Pera","Cor": "amarela"}
+var fruta3 = {"nome":"laranja","Cor": "Laranja"}
+frutas.push(fruta1,fruta2,fruta3);
+console.log(frutas.length);
+console.log(frutas.map(x => x.nome));
+console.log(frutas[0].nome)
+*/
+/*
+var frutas = ["maça","pera","laranja"];
+console.log(frutas[0]);
+frutas.push("uva")
+console.log(frutas.length);
+console.log(frutas.toString());
+console.log(frutas.join(" - "));
+console.log(frutas.reverse().join(" - "));
+*/
+
+/*
+var idade = 48;
+var idade2 = 10;
+var nome = "Marcos Ribeiro";
 //alert("Meu primeiro JS")
 console.log(idade + idade2);
 console.log(nome + " tem "+idade+" anos de idade");
 console.log(idade2*3);
+*/
